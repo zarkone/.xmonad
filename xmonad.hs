a3898fb88c4f9ae48259876bea029629c3472a43
Anatolii Smolianinov <zarkonesmall@gmail.com>
2020-10-20 15:50:59 +0200

    change terminal, browser; brightness keys adjustments

--- xmonad.hs
+++ xmonad.hs
@@ -12,7 +12,7 @@ main = do
       { modMask            = mod4Mask
       , normalBorderColor = "#221122"
       , focusedBorderColor = "#664466"
-      , terminal           = "urxvt"
+      , terminal           = "alacritty"
       , manageHook         = manageDocks <+> manageHook defaultConfig
       , layoutHook         = avoidStruts  $ layoutHook defaultConfig
       -- this must be in this order, docksEventHook must be last
@@ -29,11 +29,13 @@ main = do
       , ("M-C-b", sendMessage ToggleStruts)
       , ("M-b", banishScreen LowerRight)
       , ("M-e", spawn "emacsclient -ca ''")
-      , ("M-w", spawn "firefox")
+      , ("M-w", spawn "google-chrome-stable")
       , ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +1.5%")
       , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@  -1.5%")
       , ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-      , ("<XF86MonBrightnessUp>", spawn "light -A 100")
-      , ("<XF86MonBrightnessDown>", spawn "light -U 90")
+      , ("<XF86MonBrightnessUp>", spawn "light -A 20")
+      , ("<XF86MonBrightnessDown>", spawn "light -U 20")
+      , ("C-<XF86AudioRaiseVolume>", spawn "light -A 20")
+      , ("C-<XF86AudioLowerVolume>", spawn "light -U 20")
 
       ]
